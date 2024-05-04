53abe29d3f66e108eac8e674ba39a39c8157e233
enbraining <me@xolving.com>
2024-05-05 00:09:16 +0900

    traffic k6

---
+++ Traffic/k6/socket.js
@@ -0,0 +1,58 @@
+import { check } from 'k6';
+import ws from 'k6/ws';
+
+export default function () {
+  const url = 'ws://localhost:8080/ws/chat';
+
+  const response = ws.connect(url, function (socket) {
+    socket.on('open', function open() {
+      console.log('connected');
+      socket.send(`{"message":"hello"}`);
+      socket.send(`{"message":"hello"}`);
+      socket.send(`{"message":"hello"}`);
+      socket.send(`{"message":"hello"}`);
+      socket.send(`{"message":"hello"}`);
+    
+
+      socket.setInterval(function timeout() {
+        socket.ping();
+        console.log('Pinging every 1sec (setInterval test)');
+      }, 1000);
+    });
+
+    socket.on('ping', function () {
+      console.log('PING!');
+    });
+
+    socket.on('pong', function () {
+      console.log('PONG!');
+    });
+
+    socket.on('pong', function () {
+      // Multiple event handlers on the same event
+      console.log('OTHER PONG!');
+    });
+
+    socket.on('message', function (message) {
+      console.log(`Received message: ${message}`);
+    });
+
+    socket.on('close', function () {
+      console.log('disconnected');
+    });
+
+    socket.on('error', function (e) {
+      if (e.error() != 'websocket: close sent') {
+        console.log('An unexpected error occured: ', e.error());
+      }
+    });
+
+    socket.setTimeout(function () {
+      console.log('2 seconds passed, closing the socket');
+      socket.close();
+    }, 2000);
+  });
+
+  check(response, { 'status is 101': (r) => r && r.status === 101 });
+}
+//VU execution won't be completely finished until the connection is closed.
